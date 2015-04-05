1d5835a87a576c5120027d1dfc7403639c39a606
monnalisa <monna.lisa94@gmail.com>
2015-04-05 22:58:21 +0200

    Nowe :)

---
+++ Juwenaliowa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Juwenaliowa
+//
+//  Created by Monika Kastner on 05.04.2015.
+//  Copyright (c) 2015 Monika Kastner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
